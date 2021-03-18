4e3b961c7c37de966fe5925d133b25263a18918f
azly <azlymusa@gmail.com>
2021-03-18 21:24:35 +0800

    commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,20 @@
+package samples.demo;
+
+public class Location {
+	
+	private String username;
+	
+	public static void main (String[] args) {
+		System.out.println("username");
+		
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+}
