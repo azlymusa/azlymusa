27a538455e8c24d871afcaa4d195266a325962de
azly <azlymusa@gmail.com>
2021-03-18 21:31:27 +0800

    new modified

--- samples/src/samples/demo/Location.java
+++ samples/src/samples/demo/Location.java
@@ -3,6 +3,8 @@ package samples.demo;
 public class Location {
 	
 	private String username;
+	private int id;
+	private String age;
 	
 	public static void main (String[] args) {
 		System.out.println("username");
